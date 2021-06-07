968652fb69e53987668ef36e778c9417f96e28c7
venustuspl <venustus.pl@gmail.com>
2021-06-07 20:56:09 +0200

    first commit

---
+++ src/main/java/pl/venustus/Test2021/Test2021Application.java
@@ -0,0 +1,13 @@
+package pl.venustus.Test2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/venustus/Test2021/Test2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.Test2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
